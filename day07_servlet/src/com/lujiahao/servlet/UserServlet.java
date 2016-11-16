3be9946096920946793d346fbe7aef0153829a39
chiahaolu <xinruodingshui@gmail.com>
2016-11-16 13:47:58 +0800

    添加第七天课程项目工程

---
+++ day07_servlet/src/com/lujiahao/servlet/UserServlet.java
@@ -0,0 +1,34 @@
+package com.lujiahao.servlet;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+/**
+ * Created by lujiahao on 2016/11/16.
+ */
+public class UserServlet implements Servlet {
+    @Override
+    public void init(ServletConfig servletConfig) throws ServletException {
+
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        return null;
+    }
+
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+
+    }
+
+    @Override
+    public String getServletInfo() {
+        return null;
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
